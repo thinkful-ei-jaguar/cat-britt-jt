eb0cd8e82094389bfddedd1a2231a698b12c9762
JTLiner925 <55215116+JTLiner925@users.noreply.github.com>
2019-12-04 11:20:18 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-$('.thumbnail').on('click', e => {
-    console.log(e.target);
+'use strict';
+function handleThumbnailClicks() {
+  $('.thumbnail').on('click', function(event) {
+    const imgSrc = $(event.currentTarget).find('img').attr('src');  
+    const imgAlt = $(event.currentTarget).find('img').attr('alt');
+      
+    $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+      
+    // Accomplishes the same as line 6 with only one use off .attr()
+    // by passing it an object with multiple properties.
+    // See: http://api.jquery.com/attr/#attr-attributes
+    // $('.hero img').attr({'src': imgSrc, 'alt': imgAlt});
   });
+}
   
+$(handleThumbnailClicks);
